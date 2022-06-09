f1456b0c9b39006ec4e2bccffa16ac55af7c050a
Naman Bansal <namanaaditya0611@gmail.com>
2022-06-09 16:40:09 +0530

    Completed Hero Section

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<div class="bg-green-900 h-screen flex flex-col">
+	<div class="bg-green-900 flex flex-col pb-10">
 		<div class="flex flex-col">
 			<div class="p-5 text-center">
 				<a href="#" class="lg: text-center text-4xl font-Raleway text-white font-semibold hover:text-opacity-75">Duelance</a>
@@ -18,18 +18,49 @@
 			<div></div>
 			<button class="grow-1 text-white font-Montserrat font-semibold text-xl mt-5 p-3 hover:bg-teal-900">View Features</button>
 		</div>
-		<div class="flex flex-row justify-center mt-10">
-			<div class="flex flex-col bg-teal-700 rounded-lg">
-				<h2 class="text-white font-Raleway font-semibold text-4xl px-5 pt-4">$8900</h2>
-				<h3 class="text-white font-Raleway font-semibold text-xl px-5">Invoices Sent</h3>
-				<h2 class="text-white font-Raleway font-semibold text-4xl px-5 pt-4">$6400</h2>
-				<h3 class="text-white font-Raleway font-semibold text-xl px-5 pb-5">Money Received</h3>
+		<div class="flex flex-row justify-evenly mt-10 flex-wrap">
+			<div class="flex flex-col bg-teal-700 rounded-lg justify-evenly">
+				<p class="text-white font-Montserrat font-semibold text-4xl px-5 pt-4">$8900</p>
+				<p class="text-white font-Raleway font-semibold text-xl px-5">Invoices Sent</p>
+				<p class="text-white font-Montserrat font-semibold text-4xl px-5 pt-4">$6400</p>
+				<p class="text-white font-Raleway font-semibold text-xl px-5 pb-5">Money Received</p>
 			</div>
-			<div class="flex flex-col bg-teal-700 rounded-lg">
+
+			<div class="flex flex-col bg-teal-700 rounded-lg justify-evenly">
+				<p class="text-white font-Raleway font-semibold text-xl px-5 pt-4">EXPENSES</p>
+				<div class="flex flex-row">
+					<p class="text-white font-Montserrat font-semibold text-5xl px-3 pt-4">$923</p>
+					<div class="bg-green-600 text-white font-Montserrat p-1 h-1/2 mt-6 mr-3">+3.5%</div>
+				</div>
+				<div class="flex flex-row justify-between py-10 px-5">
+					<div class="w-1/6 h-20 bg-yellow-400"></div>
+					<div class="w-1/6 mt-10 h-10 bg-green-400"></div>
+					<div class="w-1/6 mt-5 h-15 bg-yellow-400"></div>
+					<div class="w-1/6 h-18 mt-2 bg-green-400"></div>
+					<div class="w-1/6 h-10 mt-10 bg-yellow-400"></div>
+				</div>
+			</div>
+			<div class="flex flex-col bg-teal-700 rounded-lg p-5 justify-evenly">
+				<p class="text-2xl font-Raleway text-white font-semibold">Projects Due</p>
+				<p class="text-2xl font-Raleway text-white font-semibold">This Month </p>
+				<div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Not Started - 4</div>				
+				<div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">In Progress - 2</div>				
+				<div class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Completed - 0</div>				
+			</div>
+			<div class="flex flex-col bg-teal-700 rounded-lg p-5">
+				<p class="text-2xl font-Raleway text-white font-semibold">Technical Blog</p>
+				<p class="text-2xl font-Raleway text-white font-semibold">On Kubernetes</p>
 				
+				<div class="w-full border-t border-gray-300 m-auto p-2"></div>
+				<p class="text-xl font-Montserrat text-white font-semibold">Client #1</p>
+				<div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 w-3/4">In Progress</div>
+				<div class="flex flex-row flex-wrap items-center mt-3">
+					<div class="border-4 border-white rounded-full mr-3">
+						<img src="https://assets.website-files.com/61ee3797b5357cac8a845d07/61ee3797b5357c4cb5845e4f_Avatar%2001.png" loading="lazy" alt="">
+					</div>
+					<p class="font-xl text-white font-Montserrat font-medium">Stephanie</p>
+				</div>
 			</div>
-			<div class="flex flex-col bg-teal-700 rounded-lg"></div>
-			<div class="flex flex-col bg-teal-700 rounded-lg"></div>
 		</div>
 	</div>
 </main>
