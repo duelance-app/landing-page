92063eaa805f69f453e602d2fae726de16172f03
Naman Bansal <namanaaditya0611@gmail.com>
2022-06-11 16:40:39 +0530

    Configured email marketing service and added working waitlist signup form

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-
+	
 </script>
 
 <main>
 	<div class="bg-green-900 flex flex-col pb-10">
 		<div class="flex flex-col">
 			<div class="p-5 text-center">
-				<a href="##" class="lg: text-center text-4xl font-Raleway text-white font-semibold hover:text-opacity-75">Duelance</a>
+				<a href="#" class="lg: text-center text-4xl font-Raleway text-white font-semibold hover:text-opacity-75">Duelance</a>
 			</div>
 			<div class="w-full border-t border-gray-300 m-auto"></div>
 		</div>
@@ -76,13 +76,13 @@
 		<section class="p-4">
 		  <div class="flex flex-wrap text-center">
 			<div class="md:w-1/2 py-4 md:px-10 mb-4 md:mb-0">
-				<img class="mx-auto mb-3 w-40"
-				src="images/handshake-icon.png" alt="">
+				<img class="mx-auto mb-3 w-4/5"
+				src="images/app-dashboard-duelance.png" alt="">
 			  <h3 class="text-2xl mb-3 font-heading text-gray-800 font-medium">All-In-One Management Solution For Your Freelance Business.</h3>
 			  <p class="text-gray-500 leading-relaxed">As a freelancer, you've got a lot on your plate already, along with stupid tasks like project management, invoicing, accounting, etc. Duelance is one platform that helps you manage all these aspects of your freelance business, from one place. No more shifting between ten different tools for ten different tasks.</p>
 			</div>
 			<div class="md:w-1/2 py-4 md:px-10 mb-4 md:mb-0 md:border-l">
-				<img class="w-full mx-auto" src="images/app-dashboard-duelance.png" alt="">
+				<div class="sender-form-field text-center mx-auto" data-sender-form-id="l49pypfgbq66dd622i1"></div>
 			</div>
 		  </div>
 		</section>
@@ -107,129 +107,18 @@
 		  </div>
 		</section>
 	  </div>
-	  <div class="min-w-screen min-h-screen bg-[#f0932b] flex items-center p-5 lg:p-20 overflow-hidden relative">
-  	<div class="border-2 relative rounded-xl flex flex-col md:flex-row items-center h-96  lg:h-80 sm:px-12 mx-auto w-full">
-    <div class="z-0 order-1 md:order-2 relative w-full md:w-2/5 h-80 overflow-hidden rounded-lg md:rounded-none md:rounded-r-lg">
-      <div class="absolute  bottom-5 right-20 z-50">
-        <img
-          src="https://ouch-cdn2.icons8.com/DoiXyEPMMfoX1cMkPaoV3iutZrsro-bFiQ7x1INDdqk/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNzY3/LzU3MDUzNGJkLTBm/YmQtNGEwMi05NTFi/LWFmMjlkNmI0MjFk/YS5wbmc.png"
-          alt="icon 1" class="">
-      </div>
 
-      <div class="absolute inset-0 w-full h-full object-fill object-center bg-blue-400 bg-opacity-30 bg-cover bg-bottom"
-        style="background-image: url( https://images.unsplash.com/photo-1589859762194-eaae75c61f64?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGJhY2tncm91bmQlMjBzb2xpZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=80); background-blend-mode: multiply;">
-      </div>
-      <div
-        class="md:hidden absolute inset-0 h-full p-6 pb-6 flex flex-col-reverse justify-start items-start bg-gradient-to-b from-transparent via-transparent to-gray-900">
-
-      </div>
-      <svg class="hidden md:block absolute inset-y-0 h-full w-24 fill-current text-white -ml-12" viewBox="0 0 100 100"
-        preserveAspectRatio="none">
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
-    </div>
-
-    <div class="z-10 order-2 md:order-1 w-full h-full md:w-3/5 flex items-center -mt-6 md:mt-0">
-      <div
-        class="p-8 md:pr-18 md:pl-14 md:py-12 mx-2 md:mx-0 h-full bg-white rounded-lg md:rounded-none md:rounded-l-lg shadow-xl md:shadow-none px-12">
-        <h1 class="md:block font-bold text-4xl text-purple-600 mb-2">Hey, wait...</h1>
-        <h2 class="md:block font-semibold text-2xl text-gray-700 mb-4">Join the waitlist!</h2>
-        <p class="text-gray-600 text-justify">You will be the first to be notified about the product launch and get early access to it! You do wanna improve your freelance business?</p>
-        <form class="max-w-md items-center justify-center flex my-5 mx-auto mt-8">
-          <input type="email" placeholder="email@example.com"
-            class="text-gray-500 w-full pl-4 pr-14 py-3 rounded-l-xl bg-gray-200 focus:outline-none border-transparent focus:border-transparent focus:ring-0 " />
-          <button
-            class="w-1/3 sm:px-5 py-3 rounded-r-xl rounded-bl-3xl text-white focus:outline-none border-transparent focus:border-transparent focus:ring-0 bg-indigo-800  sm:mt-0 -ml-12 sm:w-auto ">
-            Join
-          </button>
-        </form>
-      </div>
-    </div>
-
-  </div>
-	</div>
 	<footer class="my-12 pt-5 bg-green-900">
-		<div class="flex flex-col sm:flex-row justify-between pt-8 mx-8">
-		  <div class="w-full sm:w-1/2">
-			<h1 class="font-Raleway text-4xl text-gray-100 font-bold sm:text-5xl">Duelance</h1>
-			<div class="mt-4">
-			  <a href="#" class="mx-1 inline-block pt-1">
-				<svg class="w-7 h-7 text-gray-200 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg"
-				  viewBox="0 0 24 24">
-				  <path
-					d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-				</svg>
-			  </a>
-			  <a href="#" class="mx-1 inline-block pt-1">
-				<svg class="w-7 h-7 text-gray-200 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg"
-				  viewBox="0 0 24 24">
-				  <path
-					d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-				</svg>
-			  </a>
-			  <a href="#" class="mx-1 inline-block pt-1">
-				<svg class="w-7 h-7 text-gray-200 hover:text-gray-100 fill-current" viewBox="0 0 24 24"
-				  xmlns="http://www.w3.org/2000/svg">
-				  <path
-					d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z" />
-				</svg>
-			  </a>
-			  <a href="#" class="mx-1 inline-block pt-1">
-				<svg class="w-7 h-7 text-gray-200 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg"
-				  xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"
-				  preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-				  <g fill="none">
-					<path fill-rule="evenodd" clip-rule="evenodd"
-					  d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z"
-					  fill="currentColor" />
-				  </g>
-				</svg>
-			  </a>
-			  <a href="#" class="mx-1 inline-block pt-1">
-				<svg class="w-7 h-7 text-gray-200 hover:text-gray-100 fill-current" xmlns="http://www.w3.org/2000/svg"
-				  viewBox="0 0 16 16">
-				  <path
-					d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
-				  </svg>
-			  </a>
-			</div>
-		  </div>
-		  <div class="w-full sm:w-1/2 ">
-			<div class="flex flex-row space-x-10 sm:justify-end mt-8 sm:mt-0">
-			  <div class="mb-5">
-				<h4 class="pb-4 font-bold uppercase text-gray-200">Explore</h4>
-				<ul class="text-gray-300">
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">About</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Careers</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Forum</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Blog</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Community</a></li>
-				</ul>
-			  </div>
-			  <div class="mb-5">
-				<h4 class="pb-4 font-bold uppercase text-gray-200">Company</h4>
-				<ul class="text-gray-300">
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Security</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Terms of Use</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Contact Us</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">FAQ</a></li>
-				  <li class="pb-4"> <a href="#" class="hover:text-gray-200">Imprint</a></li>
-			  </div>
-			</div>
-		  </div>
-		</div>
-		<hr class="h-px mt-6 mx-6 bg-gray-100 border-none">
 		<div class="w-full px-10 rounded-b-lg">
 		  <div class="max-w-7xl flex flex-col-reverse sm:flex-row py-4 mx-auto justify-between items-center">
 			<div class="text-center mt-4 sm:mt-0 text-gray-100">
 			  &copy; 2022 <span class="font-bold text-gray-50">Duelance</span>.
 			</div>
+			<div class="">
+				<p class="font-Raleway text-xl text-gray-100 font-bold sm:text-5xl">Duelance</p>
+			</div>
 			<div class="text-center text-xl text-gray-100">
-			  <ul class="w-full justify-center text-sm flex text-center flex-row mx-auto space-x-2 overflow-hidden">
-				<li><button class="border-none hover:text-gray-50">Privacy Policy</button></li>
-				<span>|</span>
-				<li><button class="border-none hover:text-gray-50">Terms of Service</button></li>
-			  </ul>
+				<p class="font-Raleway font-xl font-semibold">Coming soon...</p>
 			</div>
 		  </div>
 		</div>
