3890579081993eaa7a44da2f1c87fe7de4f5f122
Naman Bansal <naman@duelance.app>
2022-06-16 19:50:59 +0530

    Fixed box sizing

--- src/App.svelte
+++ src/App.svelte
@@ -15,24 +15,24 @@
 			<h1 class="font-semibold text-white text-7xl font-Raleway text-center pt-3">Freelance Business.</h1>
 		</div>
 		<div class="flex flex-row justify-evenly mt-10 flex-wrap mx-2">
-			<div class="flex bg-blue-400 flex-col bg- rounded-lg justify-evenly mb-8">
-				<p class="text-black font-Raleway font-semibold text-xl px-5">Invoices Sent</p>
-				<div class="flex flex-row flex-wrap items-center">
-					<p class="text-black font-Montserrat font-semibold text-4xl px-5">$8900</p>
-					<div class="bg-green-700 text-white font-Montserrat p-1 h-3/4 mr-3">+42.6%</div>
+			<div class="flex bg-blue-400 flex-col rounded-lg justify-evenly mb-8" id="invoice-money-box">
+				<p class="text-black font-Raleway font-semibold text-xl px-5" id="invoices-sent">Invoices Sent</p>
+				<div class="flex flex-row flex-wrap items-start">
+					<p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2" id="money-received">$8900</p>
+					<div class="bg-green-700 text-white font-Montserrat p-1 h-5/12" id="percentage-money-received">+42.6%</div>
 				</div>
 				<p class="text-black font-Raleway font-semibold text-xl px-5">Money Received</p>
 				<div class="flex flex-row flex-wrap items-start">
-					<p class="text-black font-Montserrat font-semibold text-4xl px-5 pb-5">$6400</p>
+					<p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2 pb-5">$6400</p>
 					<div class="bg-green-700 text-white font-Montserrat p-1 h-1/2 mr-3 mt-2">+22.4%</div>
 				</div>
 			</div>
 
 			<div class="flex flex-col bg-blue-400 rounded-lg justify-evenly mb-8">
-				<p class="text-black font-Raleway font-semibold text-xl px-5 pt-4">EXPENSES</p>
-				<div class="flex flex-row">
+				<p class="text-black font-Raleway font-semibold text-xl px-3 pt-4">EXPENSES</p>
+				<div class="flex flex-row mr-3">
 					<p class="text-black font-Montserrat font-semibold text-5xl px-3 pt-4">$923</p>
-					<div class="bg-red-700 text-white font-Montserrat p-1 h-1/2 mt-6 mr-3">+3.5%</div>
+					<div class="bg-red-700 text-white font-Montserrat p-1 h-1/2 mt-6 mr-2">+3.5%</div>
 				</div>
 				<div class="flex flex-row justify-between py-10 px-5">
 					<div class="w-1/6 h-20 bg-blue-900"></div>
@@ -42,10 +42,10 @@
 					<div class="w-1/6 h-10 mt-10 bg-yellow-600"></div>
 				</div>
 			</div>
-			<div class="flex flex-col bg-blue-400 rounded-lg p-5 justify-evenly mb-8">
+			<div class="flex flex-col bg-blue-400 rounded-lg p-5 justify-evenly mb-8 px-7">
 				<p class="text-2xl font-Raleway text-black font-semibold">Projects Due</p>
 				<p class="text-2xl font-Raleway text-black font-semibold">This Month </p>
-				<div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Not Started - 4</div>				
+				<div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md">Not Started - 4</div>				
 				<div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">In Progress - 2</div>				
 				<div class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Completed - 0</div>				
 			</div>
@@ -131,5 +131,14 @@
 <style>
 	a {
 		text-decoration: none;
+	}
+	@media(max-width: 415px) {
+		#invoice-money-box {
+			padding-top: 1.2em;
+			padding-bottom: 1.2em;
+		}
+		#money-received {
+			margin-bottom: 0.8em;
+		}
 	}
 </style>
