06f2ca6bb60ea3204240c6e4cf8188cc8982b0a1
Naman Bansal <naman@duelance.app>
2022-06-17 17:03:08 +0530

    Component extraction of Hero Boxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,16 @@
 <script lang="ts">
 	import Navbar from "./components/Navbar.svelte"; 
+	import HeroBoxes from "./components/HeroBoxes.svelte";
 </script>
 
 <main>
 	<div class="bg-gray-900 flex flex-col pb-10">
 		<Navbar />
-		<!-- <div class="flex flex-col">
-			<div class="p-5 text-center">
-				<a href="https://duelance.app" class="lg: text-center text-4xl font-Raleway text-white font-semibold hover:text-opacity-75">Duelance</a>
-			</div>
-			<div class="w-full border-t border-gray-300 m-auto"></div>
-		</div> -->
 		<div class="flex flex-col items-center">
 			<h1 class="font-semibold text-white text-6xl font-Raleway text-center pt-7">Stay On Top Of Your</h1>
 			<h1 class="font-semibold text-white text-7xl font-Raleway text-center pt-3">Freelance Business.</h1>
 		</div>
-		<div class="flex flex-row justify-evenly mt-10 flex-wrap mx-2">
-			<div class="flex bg-blue-400 flex-col rounded-lg justify-evenly mb-8" id="invoice-money-box">
-				<p class="text-black font-Raleway font-semibold text-xl px-5" id="invoices-sent">Invoices Sent</p>
-				<div class="flex flex-row flex-wrap items-start">
-					<p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2" id="money-received">$8900</p>
-					<div class="bg-green-700 text-white font-Montserrat p-1 h-5/12" id="percentage-money-received">+42.6%</div>
-				</div>
-				<p class="text-black font-Raleway font-semibold text-xl px-5">Money Received</p>
-				<div class="flex flex-row flex-wrap items-start">
-					<p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2 pb-5">$6400</p>
-					<div class="bg-green-700 text-white font-Montserrat p-1 h-1/2 mr-3 mt-2">+22.4%</div>
-				</div>
-			</div>
-
-			<div class="flex flex-col bg-blue-400 rounded-lg justify-evenly mb-8">
-				<p class="text-black font-Raleway font-semibold text-xl px-3 pt-4">EXPENSES</p>
-				<div class="flex flex-row mr-3">
-					<p class="text-black font-Montserrat font-semibold text-5xl px-3 pt-4">$923</p>
-					<div class="bg-red-700 text-white font-Montserrat p-1 h-1/2 mt-6 mr-2">+3.5%</div>
-				</div>
-				<div class="flex flex-row justify-between py-10 px-5">
-					<div class="w-1/6 h-20 bg-blue-900"></div>
-					<div class="w-1/6 mt-10 h-10 bg-yellow-600"></div>
-					<div class="w-1/6 mt-5 h-15 bg-green-900"></div>
-					<div class="w-1/6 h-18 mt-2 bg-blue-900"></div>
-					<div class="w-1/6 h-10 mt-10 bg-yellow-600"></div>
-				</div>
-			</div>
-			<div class="flex flex-col bg-blue-400 rounded-lg p-5 justify-evenly mb-8 px-7">
-				<p class="text-2xl font-Raleway text-black font-semibold">Projects Due</p>
-				<p class="text-2xl font-Raleway text-black font-semibold">This Month </p>
-				<div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md">Not Started - 4</div>				
-				<div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">In Progress - 2</div>				
-				<div class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Completed - 0</div>				
-			</div>
-			<div class="flex flex-col bg-blue-400 rounded-lg p-5 mb-8">
-				<p class="text-2xl font-Raleway text-black font-semibold">Technical Blog</p>
-				<p class="text-2xl font-Raleway text-black font-semibold">On Kubernetes</p>
-				
-				<div class="w-full border-t border-black m-auto p-2"></div>
-				<p class="text-xl font-Montserrat text-black font-semibold">Client #1</p>
-				<div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 w-3/4">In Progress</div>
-				<div class="flex flex-row flex-wrap items-center mt-3">
-					<div class="border-4 border-white rounded-full mr-3">
-						<img src="https://assets.website-files.com/61ee3797b5357cac8a845d07/61ee3797b5357c4cb5845e4f_Avatar%2001.png" loading="lazy" alt="">
-					</div>
-					<p class="font-xl text-black font-Montserrat font-medium">Stephanie</p>
-				</div>
-			</div>
-		</div>
+		<HeroBoxes />
 	</div>
 	<div class="px-12 mx-auto bg-gradient-to-r from-gray-800 to-gray-900">
 		<section class="py-12 px-4 text-center">
@@ -110,140 +56,6 @@
 		  </div>
 		</section>
 		<hr class="border border-gray-300 w-full">
-	  	<div>
-            <div class="w-full border-t border-black m-auto p-2" />
-                <p class="text-xl font-Montserrat text-black font-semibold">
-                    Client #1
-                </p>
-                <div
-                    class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 w-3/4"
-                >
-                    In Progress
-                </div>
-                <div class="flex flex-row flex-wrap items-center mt-3">
-                    <div class="border-4 border-white rounded-full mr-3">
-                        <img
-                            src="https://assets.website-files.com/61ee3797b5357cac8a845d07/61ee3797b5357c4cb5845e4f_Avatar%2001.png"
-                            loading="lazy"
-                            alt=""
-                        />
-                    </div>
-                    <p class="font-xl text-black font-Montserrat font-medium">
-                        Stephanie
-                    </p>
-                </div>
-            </div>
-        </div>
-    <div class="px-12 mx-auto bg-gradient-to-r from-gray-800 to-gray-900">
-        <section class="py-12 px-4 text-center">
-            <div class="w-full text-center">
-                <h1 class="text-5xl leading-tight font-bold text-white">
-                    What We're Building
-                </h1>
-                <p class="mt-6 text-xl leading-relaxed text-white">
-                    Have a 👀 at how our product will change your freelancing
-                    business!
-                </p>
-                <button
-                    class="text-white my-6 py-2 px-8 border-2 rounded-full hover:text-gray-200 hover:bg-blue-400 border-blue-400"
-                    >Join The Waitlist</button
-                >
-            </div>
-        </section>
-        <hr class="border border-gray-300" />
-        <section class="p-4">
-            <div class="flex flex-wrap text-center">
-                <div class="md:w-1/2 py-4 md:px-10 mb-4 md:mb-0">
-                    <img
-                        class="mx-auto mb-3 w-4/5"
-                        src="images/duelance-ui-update-2.png"
-                        alt=""
-                    />
-                    <h3
-                        class="text-white text-2xl mb-3 font-heading font-medium"
-                    >
-                        All-In-One Management Solution For Your Freelance
-                        Business.
-                    </h3>
-                    <p class="text-white leading-relaxed">
-                        As a freelancer, you've got a lot on your plate already,
-                        along with stupid tasks like project management,
-                        invoicing, accounting, etc. Duelance is one platform
-                        that helps you manage all these aspects of your
-                        freelance business, from one place. No more shifting
-                        between ten different tools for ten different tasks.
-                    </p>
-                </div>
-                <div class="md:w-1/2 py-4 md:px-10 mb-4 md:mb-0 md:border-l">
-                    <div
-                        style="text-align: center"
-                        class="sender-form-field"
-                        data-sender-form-id="l49pypfgbq66dd622i1"
-                    />
-                </div>
-            </div>
-        </section>
-        <hr class="border border-gray-300" />
-        <section class="py-8 px-4">
-            <div class="flex flex-wrap -mx-4 -mb-6">
-                <div class="lg:w-1/3 px-4 mb-6">
-                    <img
-                        class="w-12 mb-2"
-                        src="https://cdn-icons-png.flaticon.com/128/2916/2916103.png"
-                        alt=""
-                    />
-                    <h3
-                        class="text-2xl mb-3 font-heading text-white font-medium"
-                    >
-                        Accounting & Invoicing
-                    </h3>
-                    <p class="text-white leading-relaxed">
-                        Tools to easily track payments, expenses, revenue,
-                        profits, and more. Send beautiful invoices and receive
-                        payments in your bank directly. Stay on top of your
-                        numbers.
-                    </p>
-                </div>
-                <div class="lg:w-1/3 px-4 mb-6 ">
-                    <img
-                        class="w-12 mb-2"
-                        src="https://cdn-icons-png.flaticon.com/128/2190/2190552.png"
-                        alt=""
-                    />
-                    <h3
-                        class="text-2xl mb-3 font-heading text-white font-medium"
-                    >
-                        Client Relationships
-                    </h3>
-                    <p class="text-white leading-relaxed">
-                        They say, "Clients are God". Easily manage all the
-                        clients you're working for, and never forget a single
-                        detail about them.
-                    </p>
-                </div>
-                <div class="lg:w-1/3 px-4 mb-6">
-                    <img
-                        class="w-12 mb-2"
-                        src="https://cdn-icons-png.flaticon.com/128/1159/1159283.png"
-                        alt=""
-                    />
-                    <h3
-                        class="text-2xl mb-3 font-heading text-white font-medium"
-                    >
-                        Project Management
-                    </h3>
-                    <p class="text-white leading-relaxed">
-                        Gone are the days of missing project due dates. Track
-                        all the things you're working on, their status, and
-                        more, in one place so that you can stay managed and
-                        sane.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <hr class="border border-gray-300 w-full" />
-    </div>
-
     <footer class="bg-gray-900">
         <div class="w-full px-10 rounded-b-lg">
             <div
@@ -272,13 +84,5 @@
 </main>
 
 <style>
-	@media(max-width: 415px) {
-		#invoice-money-box {
-			padding-top: 1.2em;
-			padding-bottom: 1.2em;
-		}
-		#money-received {
-			margin-bottom: 0.8em;
-		}
-	}
+	
 </style>
---
+++ src/components/HeroBoxes.svelte
@@ -0,0 +1,66 @@
+<script lang="ts"></script>
+
+<main>
+    <div class="flex flex-row justify-evenly mt-10 flex-wrap mx-2">
+        <div class="flex bg-blue-400 flex-col rounded-lg justify-evenly mb-8" id="invoice-money-box">
+            <p class="text-black font-Raleway font-semibold text-xl px-5" id="invoices-sent">Invoices Sent</p>
+            <div class="flex flex-row flex-wrap items-start">
+                <p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2" id="money-received">$8900</p>
+                <div class="bg-green-700 text-white font-Montserrat p-1 h-5/12" id="percentage-money-received">+42.6%</div>
+            </div>
+            <p class="text-black font-Raleway font-semibold text-xl px-5">Money Received</p>
+            <div class="flex flex-row flex-wrap items-start">
+                <p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2 pb-5">$6400</p>
+                <div class="bg-green-700 text-white font-Montserrat p-1 h-1/2 mr-3 mt-2">+22.4%</div>
+            </div>
+        </div>
+
+        <div class="flex flex-col bg-blue-400 rounded-lg justify-evenly mb-8">
+            <p class="text-black font-Raleway font-semibold text-xl px-3 pt-4">EXPENSES</p>
+            <div class="flex flex-row mr-3">
+                <p class="text-black font-Montserrat font-semibold text-5xl px-3 pt-4">$923</p>
+                <div class="bg-red-700 text-white font-Montserrat p-1 h-1/2 mt-6 mr-2">+3.5%</div>
+            </div>
+            <div class="flex flex-row justify-between py-10 px-5">
+                <div class="w-1/6 h-20 bg-blue-900"></div>
+                <div class="w-1/6 mt-10 h-10 bg-yellow-600"></div>
+                <div class="w-1/6 mt-5 h-15 bg-green-900"></div>
+                <div class="w-1/6 h-18 mt-2 bg-blue-900"></div>
+                <div class="w-1/6 h-10 mt-10 bg-yellow-600"></div>
+            </div>
+        </div>
+        <div class="flex flex-col bg-blue-400 rounded-lg p-5 justify-evenly mb-8 px-7">
+            <p class="text-2xl font-Raleway text-black font-semibold">Projects Due</p>
+            <p class="text-2xl font-Raleway text-black font-semibold">This Month </p>
+            <div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md">Not Started - 4</div>				
+            <div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">In Progress - 2</div>				
+            <div class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Completed - 0</div>				
+        </div>
+        <div class="flex flex-col bg-blue-400 rounded-lg p-5 mb-8">
+            <p class="text-2xl font-Raleway text-black font-semibold">Technical Blog</p>
+            <p class="text-2xl font-Raleway text-black font-semibold">On Kubernetes</p>
+            
+            <div class="w-full border-t border-black m-auto p-2"></div>
+            <p class="text-xl font-Montserrat text-black font-semibold">Client #1</p>
+            <div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 w-3/4">In Progress</div>
+            <div class="flex flex-row flex-wrap items-center mt-3">
+                <div class="border-4 border-white rounded-full mr-3">
+                    <img src="https://assets.website-files.com/61ee3797b5357cac8a845d07/61ee3797b5357c4cb5845e4f_Avatar%2001.png" loading="lazy" alt="">
+                </div>
+                <p class="font-xl text-black font-Montserrat font-medium">Stephanie</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+    @media(max-width: 415px) {
+		#invoice-money-box {
+			padding-top: 1.2em;
+			padding-bottom: 1.2em;
+		}
+		#money-received {
+			margin-bottom: 0.8em;
+		}
+	}
+</style>
