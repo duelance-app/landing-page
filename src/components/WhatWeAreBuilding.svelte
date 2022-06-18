b1f50069f48354e842e2e1096594351ae87eec5f
Naman Bansal <naman@duelance.app>
2022-06-18 11:34:34 +0530

    Made signup form for waitlist

--- src/components/WhatWeAreBuilding.svelte
+++ src/components/WhatWeAreBuilding.svelte
@@ -14,7 +14,7 @@
                 </p>
                 <button
                     class="text-white my-6 py-2 px-8 border-2 rounded-full hover:text-gray-200 hover:bg-blue-400 border-blue-400"
-                    >Join The Waitlist</button
+                    > <a href="#waitlist-form" class="text-white visited:text-white">Join The Waitlist</a></button
                 >
             </div>
         </section>
@@ -42,12 +42,25 @@
                         between ten different tools for ten different tasks.
                     </p>
                 </div>
-                <div class="md:w-1/2 py-4 md:px-10 mb-4 md:mb-0 md:border-l">
-                    <div
-                        style="text-align: center"
-                        class="sender-form-field"
-                        data-sender-form-id="l49pypfgbq66dd622i1"
-                    />
+                <div class="md:w-1/2 py-4 my-auto mx-auto border-2 border-white px-4">
+                    <h3 class="text-white font-medium text-2xl">Join The Waitlist</h3>
+                    <form id="waitlist-form">
+                        <div class="mb-6 m-auto">
+                            <label for="email" class="block mb-2 text-sm font-medium text-white text-left dark:text-gray-300">Your email</label>
+                            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+                        </div>
+                        <div class="mb-6 m-auto">
+                            <label for="password" class="block mb-2 text-sm font-medium text-white text-left dark:text-gray-300">Your password</label>
+                            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                        </div>
+                        <div class="flex items-start mb-2">
+                            <div class="flex items-center h-5">
+                            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                            </div>
+                            <label for="remember" class="ml-2 text-sm font-medium text-white dark:text-gray-300">Remember me</label>
+                        </div>
+                        <button type="submit" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
                 </div>
             </div>
         </section>
@@ -113,4 +126,7 @@
 </main>
 
 <style>
+    a, a:visited {
+        text-decoration: none;
+    }
 </style>
