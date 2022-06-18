cae761ae8a525a0c0319b692184c6bf8591653bb
Naman Bansal <naman@duelance.app>
2022-06-18 22:57:33 +0530

    Fixed 4th box sizing

--- src/components/HeroBoxes.svelte
+++ src/components/HeroBoxes.svelte
@@ -3,12 +3,12 @@
 <main>
     <div class="flex flex-row justify-evenly mt-10 flex-wrap mx-2">
         <div class="flex bg-blue-400 flex-col rounded-lg justify-evenly mb-8 px-2" id="invoice-money-box">
-            <p class="text-black font-Raleway font-semibold text-xl px-5" id="invoices-sent">Invoices Sent</p>
+            <p class="text-black font-Raleway font-semibold text-xl px-5" id="invoices-sent">Requested</p>
             <div class="flex flex-row flex-wrap items-start">
                 <p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2" id="money-received">$8900</p>
                 <div class="bg-green-700 text-white font-Montserrat p-1 h-5/12" id="percentage-money-received">+42.6%</div>
             </div>
-            <p class="text-black font-Raleway font-semibold text-xl px-5">Money Received</p>
+            <p class="text-black font-Raleway font-semibold text-xl px-5">Received</p>
             <div class="flex flex-row flex-wrap items-start">
                 <p class="text-black font-Montserrat font-semibold text-4xl pl-5 pr-2 pb-5">$6400</p>
                 <div class="bg-green-700 text-white font-Montserrat p-1 h-1/2 mr-3 mt-2">+22.4%</div>
@@ -29,14 +29,14 @@
                 <div class="w-1/6 h-10 mt-10 bg-yellow-600"></div>
             </div>
         </div>
-        <div class="flex flex-col bg-blue-400 rounded-lg p-5 justify-evenly mb-8 px-4">
+        <div class="flex flex-col bg-blue-400 rounded-lg p-3 justify-evenly mb-8 px-4">
             <p class="text-2xl font-Raleway text-black font-semibold">Projects Due This</p>
             <p class="text-2xl font-Raleway text-black font-semibold">Month </p>
             <div class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md">Not Started - 4</div>				
             <div class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">In Progress - 2</div>				
             <div class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2">Completed - 0</div>				
         </div>
-        <div class="flex flex-col bg-blue-400 rounded-lg p-7 mb-8">
+        <div class="flex flex-col bg-blue-400 rounded-lg p-5 px-7 mb-8">
             <p class="text-2xl font-Raleway text-black font-semibold">Technical Blog</p>
             <p class="text-2xl font-Raleway text-black font-semibold">On Kubernetes</p>
             
