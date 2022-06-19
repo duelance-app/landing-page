94c5dec80a6cc97bef531fd75a84ff1a05cc61e9
Naman Bansal <naman@duelance.app>
2022-06-19 14:59:16 +0530

    Fixed last two boxes

--- src/components/HeroBoxes.svelte
+++ src/components/HeroBoxes.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import MdWarning from 'svelte-icons/md/MdWarning.svelte'
-
+    import MdWarning from "svelte-icons/md/MdWarning.svelte";
 
     const taxdate = new Date();
-    const dd = String(taxdate.getDate() + 1).padStart(2, '0');
-    const month = taxdate.toLocaleString('default', { month: 'long' });
-    console.log(month)
+    const dd = String(taxdate.getDate() + 1).padStart(2, "0");
+    const month = taxdate.toLocaleString("default", { month: "long" });
+    console.log(month);
 </script>
+
 <main>
     <div class="flex flex-row justify-evenly mt-10 flex-wrap mx-2">
         <div
@@ -80,48 +80,73 @@
             <p class="text-black font-Raleway font-semibold text-xl pt-3">
                 Due
             </p>
-            <p class="text-black font-Montserrat font-semibold text-4xl pt-3">
-                {dd} {month}
+            <p class="text-black font-Montserrat font-semibold text-4xl pt-3 pb-2">
+                {dd}
+                {month}
             </p>
-            <div class="flex items-center bg-red-600 text-white text-sm font-bold px-3 py-2 h-1/4 w-full rounded-3xl" role="alert">
-                <svg width="30" height="30" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div
+                class="flex items-center bg-red-600 text-white text-sm font-bold px-3 py-2 h-1/6 w-3/5 shrink "
+                role="alert"
+            >
+                <svg
+                    width="23"
+                    height="23"
+                    viewBox="0 0 47 47"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
                     <g id="Frame 1" clip-path="url(#clip0_101_20)">
-                    <path id="Polygon 4" d="M23.5 3L46.4497 40.5H0.550327L23.5 3Z" fill="black"/>
-                    <text id="!" fill="white" xml:space="preserve" style="white-space: pre" font-family="Inter" font-size="36" letter-spacing="0em"><tspan x="18" y="38.0909">!</tspan></text>
+                        <path
+                            id="Polygon 4"
+                            d="M23.5 3L46.4497 40.5H0.550327L23.5 3Z"
+                            fill="black"
+                        />
+                        <text
+                            id="!"
+                            fill="white"
+                            xml:space="preserve"
+                            style="white-space: pre"
+                            font-family="Inter"
+                            font-size="36"
+                            letter-spacing="0em"
+                            ><tspan x="18" y="38.0909">!</tspan></text
+                        >
                     </g>
                     <defs>
-                    <clipPath id="clip0_101_20">
-                    <rect width="47" height="47" fill="white"/>
-                    </clipPath>
+                        <clipPath id="clip0_101_20">
+                            <rect width="47" height="47" fill="white" />
+                        </clipPath>
                     </defs>
                 </svg>
-                <p class="text-2xl pl-2">1 Day</p>
+                <p class="text-sm pl-2">1 Day</p>
             </div>
         </div>
         <div
-            class="flex flex-col bg-blue-400 rounded-lg p-3 justify-evenly mb-8 px-5 py-5"
+            class="flex flex-col bg-blue-400 rounded-lg justify-evenly mb-8 px-7 py-5"
         >
             <p class="text-3xl font-Raleway text-black font-semibold">
                 Projects Due
             </p>
-            <p class="text-2xl font-Raleway text-black font-semibold">This Month</p>
+            <p class="text-2xl font-Raleway text-black font-semibold">
+                This Month
+            </p>
             <div
-                class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md"
+                class="bg-yellow-600 text-lg font-Montserrat text-white font-medium p-2 rounded-md w-full text-center"
             >
                 Not Started - 4
             </div>
             <div
-                class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2"
+                class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 text-center"
             >
                 In Progress - 2
             </div>
             <div
-                class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2"
+                class="bg-green-800 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 text-center"
             >
                 Completed - 0
             </div>
         </div>
-        <div class="flex flex-col bg-blue-400 rounded-lg p-5 px-7 mb-8">
+        <div class="flex flex-col bg-blue-400 rounded-lg py-5 px-7 mb-8">
             <p class="text-2xl font-Raleway text-black font-semibold">
                 Technical Blog
             </p>
@@ -129,16 +154,19 @@
                 On Kubernetes
             </p>
 
-            <div class="w-full border-t border-black m-auto p-2" />
-            <p class="text-xl font-Montserrat text-black font-semibold">
+            <div class="w-full border-t border-black mx-auto p-2" />
+            <p class="text-xl font-Montserrat text-black font-semibold pt-2">
                 Client #1
             </p>
+            <p class="text-xl font-Montserrat text-black font-semibold pt-3">
+                Payment: $315
+            </p>
             <div
-                class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-2 w-3/4"
+                class="bg-blue-900 text-lg font-Montserrat text-white font-medium p-2 rounded-md mt-4 w-3/4 text-center"
             >
                 In Progress
             </div>
-            <div class="flex flex-row flex-wrap items-center mt-3">
+            <div class="flex flex-row flex-wrap items-center mt-6">
                 <div class="border-4 border-white rounded-full mr-3">
                     <img
                         src="https://images-ext-1.discordapp.net/external/Ha03s1HrxlIcoXZ7Ep14-aVRnk4HhCRzijQKxyb-0dY/https/cdn.pixabay.com/photo/2019/10/04/13/19/woman-4525646_1280.jpg?width=50&height=50"
