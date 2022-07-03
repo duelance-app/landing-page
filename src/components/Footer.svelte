6c47013eab4ffb1274282d930f45e09b4186ab65
Naman Bansal <naman@duelance.app>
2022-07-03 12:10:28 +0530

    Added icon credit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,27 +1,35 @@
 <main>
     <footer class="bg-black">
         <div class="px-10 rounded-b-lg">
-            <div
-                class="max-w-full flex flex-col-reverse sm:flex-row py-4 mx-auto justify-between items-center"
-            >
-                <div class="text-center mt-4 sm:mt-0 text-gray-100">
-                    &copy; 2022 <span class="font-bold text-gray-50"
-                        >Duelance</span
-                    >.
+                <div
+                    class="max-w-full flex flex-col-reverse sm:flex-row py-4 mx-auto justify-between items-center"
+                >
+                    <div class="text-center mt-4 sm:mt-0 text-gray-100">
+                        &copy; 2022 <span class="font-bold text-gray-50"
+                            >Duelance</span
+                        >.
+                    </div>
+                    <div class="">
+                        <p
+                            class="font-Raleway text-5xl text-gray-100 font-bold"
+                        >
+                            Duelance
+                        </p>
+                    </div>
+                    <!-- <div class="text-center text-xl text-gray-100">
+                        <p class="font-Raleway font-xl font-semibold">
+                            Coming soon...
+                        </p>
+                    </div> -->
+                    <p class="text-white">Icon Credit: <a href="https://flaticon.com">Flaticon</a></p>
                 </div>
-                <div class="">
-                    <p
-                        class="font-Raleway text-5xl text-gray-100 font-bold"
-                    >
-                        Duelance
-                    </p>
-                </div>
-                <div class="text-center text-xl text-gray-100">
-                    <p class="font-Raleway font-xl font-semibold">
-                        Coming soon...
-                    </p>
-                </div>
-            </div>
         </div>
     </footer>
 </main>
+
+<style>
+    a {
+        text-decoration: none;
+        color: white;
+    }
+</style>
